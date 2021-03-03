4159d0aca5979f818fefe22b17d0fb97d393c46e
skaaki <55635751+skaaki@users.noreply.github.com>
2021-03-02 21:02:37 -0700

    Update GitDemo.java

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Sahaer Kaaki
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -13,8 +14,8 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
-        System.out.print("Add num to previousNumber");
-        return 0;
+        previousNumber += num;
+        return previousNumber;
     }
     
     /*****
@@ -41,7 +42,7 @@ public class GitDemo {
      *****/
     public double reverseNumber(int num){
         double newNumber = 1;
-        
+        reverseNum = newNumber;
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
